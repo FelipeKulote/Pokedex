864743457f42768c80e2b80cecc4e85d899a08c8
Felipe Augusto <lipe_augustobq@hotmail.com>
2022-07-20 18:22:12 -0300

    edições no JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,58 @@
-async function getPokemon() {
+const getPokemon = async (pokemon) => {
   const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/`);
   const data = await resposta.json();
-  console.log(data);
+//   console.log(data);
 
   data.results.forEach((item) => {
     // console.log(item);
     document.querySelector("#pokemon-list").insertAdjacentHTML(
-      "beforeend",
-      `
-        <div class="card">
-        <img class="pokemon" src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png" alt="" />
-        <div>
-           <p class="number-pokemon">001</p>
-           <h2 class="name-pokemon">${item.name}</h2>
-           <p class="type-pokemon">Grass</p>
-        </div>
-      </div>
-        `
-    );
-  });
-}
+          "beforeend",
+          `
+            <div class="card">
+            <img class="img-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="" />
+            <div>
+               <p class="number-pokemon">${item.id}</p>
+               <h2 class="name-pokemon">${item.name}</h2>
+               <p class="type-pokemon">Grass</p>
+            </div>
+          </div>
+            `
+        );
+      });  
+    }
 getPokemon();
+
+
+
+
+
+
+
 
 // pokemon.id
 // pokemon.types
+
+
+
+
+// data.results.forEach((item) => {
+//     // console.log(item);
+//     
+
+
+/* 2º jeito 
+const pokemonName = document.querySelector('.name-pokemon')
+const pokemonID = document.querySelector('.number-pokemon')
+const pokemonType = document.querySelector('.type-pokemon')
+const pokemonImg = document.querySelector('.img-pokemon')
+
+
+const renderPokemon = async (pokemon) => {
+    const data = await getPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+    pokemonID.innerHTML = data.id;
+    pokemonImg.src = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${data.id}.png`;
+    // pokemonType.innerHTML = data.type;
+}
+*/
